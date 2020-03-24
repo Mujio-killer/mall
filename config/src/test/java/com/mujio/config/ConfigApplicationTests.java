63684a96a9a814ac10b12c13371b6c331c5afc85
Mujio-killer <384373732@qq.com>
2020-03-24 22:53:34 +0800

    config完成

---
+++ config/src/test/java/com/mujio/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mujio.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
